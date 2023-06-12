2864fa6cd5256e682ec23ca8f6e4e79c6d6fe1cd
Juan Pablo Gonzalez <juanpg@gmail.com>
2023-06-12 10:39:17 -0700

    Link to Reservations page from Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import { Link, Text } from "@chakra-ui/react";
+import { Link as RouterLink } from 'react-router-dom'
 import Layout from "../Components/Layout";
 
 export default function Home() {
   return (
     <Layout>
-      This is the home page
+      <Text>This is the home page. Go to the <Link as={RouterLink} to='/reservations'>Reservations page</Link></Text>
     </Layout>
   )
 }

58b13eae47cf9a616b1b2f5cb7af3e8231d49b2e
Juan Pablo Gonzalez <juanpg@gmail.com>
2023-06-12 10:57:03 -0700

    Highlight home link to reservations

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import { Link, Text } from "@chakra-ui/react";
 import { Link as RouterLink } from 'react-router-dom'
 import Layout from "../Components/Layout";
+import { CardTitle } from "../Components/Typography";
 
 export default function Home() {
   return (
     <Layout>
-      <Text>This is the home page. Go to the <Link as={RouterLink} to='/reservations'>Reservations page</Link></Text>
+      <Text>This is the home page. Go to the <Link as={RouterLink} to='/reservations'><CardTitle>Reservations page</CardTitle></Link></Text>
     </Layout>
   )
 }
